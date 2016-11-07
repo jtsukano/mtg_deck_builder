6b0236f6f1b130bb880952fcd9f8dced36099eba
jay <jaytsukano@gmail.com>
2016-11-06 19:31:55 -0800

    change database user

--- config/database.rb
+++ config/database.rb
@@ -35,8 +35,8 @@ ActiveRecord::Base.establish_connection(
   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
   :host     => db.host,
   :port     => db.port,
-  :username => db.user = 'kzo',
-  :password => db.password = 'kansha',
+  :username => db.user,# = 'kzo',
+  :password => db.password,# = 'kansha',
   :database => DB_NAME,
   :encoding => 'utf8'
 )
